cba0fcf9e1b7a48a392e5466dca7170c7ffae9bf
Eva <luizeramiguelero@gmail.com>
2025-12-10 13:57:00 +0000

    Basic html and necessary files done

--- index.php
+++ index.php
@@ -1,11 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-PT">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
     <title>Document</title>
 </head>
 <body>
-    ajdfuewghberjgb
+    <!-- Cabeçalho do site -->
+    <header>
+
+        <!-- Menu -->
+        <nav>
+            <a href="#"></a>
+            <a href="#"></a>
+            <a href="#"></a>
+        </nav>
+    </header>
+
+    <main>
+        <!-- Introdução ao website e ferramenta -->
+        <h1>"Nome da ferramenta"</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo eos eveniet id maxime eum deserunt. Perspiciatis nihil repellendus exercitationem voluptatum velit officia amet deleniti, repellat blanditiis non consectetur quas architecto?</p>
+
+        <img src="#" alt="">
+
+        <!-- Motivos e benefícios do uso da ferramenta -->
+        <section id="UseReasons">
+            <h1>Porque utilizar?</h1>
+
+            <div id="beneficios">               
+                <h2>Beneficio 1</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil, sint repellendus velit, rem, animi maxime reprehenderit nostrum cumque molestiae asperiores laborum earum rerum. Illum id maxime vitae cupiditate? Ab?</p>
+            </div>
+
+            <div id="beneficios">
+                <h2>Beneficio 2</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil, sint repellendus velit, rem, animi maxime reprehenderit nostrum cumque molestiae asperiores laborum earum rerum. Illum id maxime vitae cupiditate? Ab?</p>
+            </div>
+
+            <div id="beneficios">
+                <h2>Beneficio 3</h2>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa nihil, sint repellendus velit, rem, animi maxime reprehenderit nostrum cumque molestiae asperiores laborum earum rerum. Illum id maxime vitae cupiditate? Ab?</p>
+            </div>
+        </section>
+
+        <!-- Um pouco do perfil profissional dos colaboradores -->
+        <section id="SocialProof">
+            <div>
+                <h1>Name</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque deleniti autem libero mollitia temporibus quisquam reprehenderit sint quasi. Omnis perferendis molestias vero veritatis corrupti, maxime reiciendis assumenda aperiam officiis fugiat.</p>
+                <img src="#" alt="">
+            </div>
+        </section>
+
+        <!-- Um breve resumo e introdução ao motivo da criação e objetivo da ferramenta -->
+        <section id="StoryTelling">
+            <h1>About us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, modi cum nemo pariatur quas, ea reprehenderit quaerat dolore culpa iste exercitationem. Quasi voluptatum ducimus architecto tenetur maiores iusto iure perspiciatis!</p>
+            <img src="#" alt="">
+        </section>
+
+        <!-- Garantias esperadas da plataforma -->
+        <section id="Guarantee">
+            <h1>Text</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni voluptatum at dolorum, deleniti porro, quisquam natus a alias modi nesciunt, reiciendis similique neque sint animi dolor. Voluptatum ut explicabo assumenda.</p>
+        </section>
+    </main>
+
+    <!-- Links úteis, texto breve de copyright e etc... -->
+    <footer>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae consequuntur, odit optio beatae eveniet reiciendis sint officia doloribus provident itaque culpa, atque, consectetur suscipit libero impedit fuga adipisci? Non!</p>
+    </footer>
+
+    <script src="JS/main.js"></script>
 </body>
 </html>
